1d23082a0c3a89328df9beb7da7e87b1d94cc813
kenthusblog <47094667+kenthusblog@users.noreply.github.com>
2021-01-10 21:28:23 +0700

    Update weton.js

--- weton.js
+++ weton.js
@@ -174,7 +174,7 @@ $('#submit').on('click', function () {
                        <center><b>`+final[0]+`</b></center>
                     <p><blockquote>
                          `+final[1]+`
-                    </blockquote></p><br/><span class="text" style="font-size: 0.7em" align="right"><a href="https://www.panjinawangkung.com">panjinawaangkung.com</a></span>
+                    </blockquote></p><br/><span class="text" style="font-size: 0.7em" align="right"><a href="https://www.panjinawangkung.com">panjinawangkung.com</a></span>
                 </div>
             </div>
         </div>
