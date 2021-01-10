9d7dee41edd251d96b5d8fc9581005c9d781b01a
kenthusblog <47094667+kenthusblog@users.noreply.github.com>
2021-01-11 03:15:02 +0700

    Update neptu.js

--- neptu.js
+++ neptu.js
@@ -64,21 +64,21 @@ function Hitung(x){
 
 
 function weton(x){
-    var pegat = [1,9,10,18,19,27,28,36,'Pegat','"Hasil pegat, bahwa menurut hitungan weton jawa kemungkinan pasangan akan sering mendapatkan masalah di kemudian hari, bisa saja masalah ekonomi, kekuasaan, perselingkuhan hingga menyebabkan pasangan bercerai."']
+    var pegat = [1,9,10,18,19,27,28,36,'PEGAT','"Hasil pegat, bahwa menurut hitungan weton jawa kemungkinan pasangan akan sering mendapatkan masalah di kemudian hari, bisa saja masalah ekonomi, kekuasaan, perselingkuhan hingga menyebabkan pasangan bercerai."']
 
-    var ratu = [2,11,20,29,'Ratu','"Hasil Ratu, menurut hitungan weton jawa bisa dikatakan bahwa pasangan ini memang sudah jodohnya. Karena didalam kehidupan nanti keluarganya akan sangat dihargai dan disegani oleh tetangga maupun masyarakat sekitar. Bahkan banyak orang yang iri hati karena keharmonisannya dalam membina rumah tangga."']
+    var ratu = [2,11,20,29,'RATU','"Hasil Ratu, menurut hitungan weton jawa bisa dikatakan bahwa pasangan ini memang sudah jodohnya. Karena didalam kehidupan nanti keluarganya akan sangat dihargai dan disegani oleh tetangga maupun masyarakat sekitar. Bahkan banyak orang yang iri hati karena keharmonisannya dalam membina rumah tangga."']
 
-    var jodo = [3,12,21,30,'Jodoh','"Hasil Jodoh, menurut hitungan weton jawa pasangan ini memang beneran cocok dan berjodoh. Karena dapat saling menerima baik kelebihan atau kekurangannya. Selain itu rumah tangganya dapat rukun sampai tua nanti."']
+    var jodo = [3,12,21,30,'JODOH','"Hasil Jodoh, menurut hitungan weton jawa pasangan ini memang beneran cocok dan berjodoh. Karena dapat saling menerima baik kelebihan atau kekurangannya. Selain itu rumah tangganya dapat rukun sampai tua nanti."']
 
-    var topo = [4,13,22,31,'Topo','"Hasil Topo, menurut hitungan jawa di gambarkan dalam membina rumah tangga nanti akan mengalami kesusahan di awal, tetapi akan bahagia di akhir nanti. Masalah ini bisa saja karena masalah ekonomi dan masih banyak lagi. Namun ketika sudah mempunyai anak dan cukup lama berumah tangga, di hari itulah kehidupanya akan menjadi sukses dan bahagia."']
+    var topo = [4,13,22,31,'TOPO','"Hasil Topo, menurut hitungan jawa di gambarkan dalam membina rumah tangga nanti akan mengalami kesusahan di awal, tetapi akan bahagia di akhir nanti. Masalah ini bisa saja karena masalah ekonomi dan masih banyak lagi. Namun ketika sudah mempunyai anak dan cukup lama berumah tangga, di hari itulah kehidupanya akan menjadi sukses dan bahagia."']
 
-    var tinari = [5, 14, 23, 32,'Tinari','"Hasil Tinari, menurut hitungan weton jawanya berarti akan menemukan kebehagaiaan dimasa nanti. Selain itu gampang dalam mencari rezeki dan sering mendapatkan keberuntungan."']
+    var tinari = [5, 14, 23, 32,'TINARI','"Hasil Tinari, menurut hitungan weton jawanya berarti akan menemukan kebehagaiaan dimasa nanti. Selain itu gampang dalam mencari rezeki dan sering mendapatkan keberuntungan."']
 
-    var padu = [6, 15, 24, 33,'Padu','"Hasil Padu, menurut hitungan weton jawa digambarkan dalam berumah tangganya nanti akan sering mengalami sebuah pertengkaran. Namun tidak sampai mengarah ke seuah perceraian. Masalah pertengkaran ini digambarkan hanya masalah yang sifatnya cukup sepele."']
+    var padu = [6, 15, 24, 33,'PADU','"Hasil Padu, menurut hitungan weton jawa digambarkan dalam berumah tangganya nanti akan sering mengalami sebuah pertengkaran. Namun tidak sampai mengarah ke seuah perceraian. Masalah pertengkaran ini digambarkan hanya masalah yang sifatnya cukup sepele."']
 
-    var sujanan = [7, 16, 25, 34,'Sujanan','"Hasil sujanan, menurut hitungan weton jawa digambarkan bahwa dalam berumah tangganya nanti akan mengalami sebuah pertengkaran, bisa saja kerana perselingkuhan yang terjadi yang di mulai dari pahak laki-laki atau si perempuan."']
+    var sujanan = [7, 16, 25, 34,'SUJANAN','"Hasil sujanan, menurut hitungan weton jawa digambarkan bahwa dalam berumah tangganya nanti akan mengalami sebuah pertengkaran, bisa saja kerana perselingkuhan yang terjadi yang di mulai dari pahak laki-laki atau si perempuan."']
 
-    var pesthi = [8, 17, 26, 35,'Pesthi','"Hasil Pesthi, menurut hitungan weton jawa digambarkan bahwa dalam berumah tangganya nanti akan selalu rukun, tenteram, adem ayem hingga tua nanti. Meskipun ada masalah, namun tidak akan bisa merusak keharmonisan keluarganya."']
+    var pesthi = [8, 17, 26, 35,'PESTHI','"Hasil Pesthi, menurut hitungan weton jawa digambarkan bahwa dalam berumah tangganya nanti akan selalu rukun, tenteram, adem ayem hingga tua nanti. Meskipun ada masalah, namun tidak akan bisa merusak keharmonisan keluarganya."']
 
 
     if(pegat.some(item => item == x)){
@@ -106,14 +106,15 @@ $('#submit').on('click', function () {
 
      //validasi nama
      var nama = $("#inputNama1").val().length;
+     var nama2 = $("#inputNama2").val().length;
      if (nama == 0) {			
-         document.getElementById('nama-alert').innerHTML = "Nama tidak boleh kosong, hehe :)";
+         document.getElementById('nama-alert').innerHTML = "Nama tidak boleh kosong :)";
          //return false;
      }else{
          document.getElementById('nama-alert').innerHTML = "";
      }
      if (nama2 == 0) {			
-         document.getElementById('nama2-alert').innerHTML = "Waduhm pasanganmu kok kosong?";
+         document.getElementById('nama2-alert').innerHTML = "Waduh Pasanganmu kok kosong?";
          //return false;
      }else{
          document.getElementById('nama2-alert').innerHTML = "";
@@ -123,13 +124,13 @@ $('#submit').on('click', function () {
     var alertTgl1 = $("#inputTanggal1").val().length;
     var alertTgl2 = $("#inputTanggal2").val().length;
         if (alertTgl1 == 0) {			
-            document.getElementById('tanggal-alert').innerHTML = "Tanggalnya diisi dulu cuy";
+            document.getElementById('tanggal-alert').innerHTML = "Tanggalnya diisi dulu gaes";
             //return false;
         }else{
             document.getElementById('tanggal-alert').innerHTML = "";
         }
         if (alertTgl2 == 0) {			
-            document.getElementById('tanggal2-alert').innerHTML = "Tanggalnya diisi dulu cuy";
+            document.getElementById('tanggal2-alert').innerHTML = "Tanggalnya diisi dulu gaes";
             //return false;
         }else{
             document.getElementById('tanggal2-alert').innerHTML = "";
@@ -137,30 +138,43 @@ $('#submit').on('click', function () {
    $("#result").html("");
 
     var nama1 = $('#inputNama1').val();
+    var nama2 = $('#inputNama2').val();	
 
     var tanggal1 = $('#inputTanggal1').val();
+    var tanggal2 = $('#inputTanggal2').val();
     
     var layla = Hitung(tanggal1);
 
+    var majnun = Hitung(tanggal2);
     
-    var total = layla[3];
+    
+	
+    var total = layla[3]+majnun[3];
     var final = weton(total)
 
     $("#result").append(`                    
-    <div class="card">
+    
+<div class="card"><div class="container">
         <div class="card-body">
             <div class="row">
-                <div class="col-11">`+nama1+`</div>
-                <div class=col-4>Tanggal </div> <div class=col-8>: `+layla[0][0]+` `+layla[0][1]+` `+layla[0][2]+`</div>
-                <div class=col-4>Pasaran </div> <div class=col-6>: `+layla[1]+ ` `+layla[2]+`</div>
-            </div>    
-               
-            <div class="row mt-3">
+                <div class="col-12"><td>Nama Kamu : <b>`+nama1+`</b></td></div>
+                <div class=col-4>Tanggal Lahir : `+layla[0][0]+` `+layla[0][1]+` `+layla[0][2]+`</div>
+                <div class=col-4>Pasaran : `+layla[1]+ ` `+layla[2]+`   </div>
+                <div class=col-4>Neptu  : `+layla[3]+`  </div>
+            </div><i class='separate'></i>    
+            <div class="row mt-2">
+                <div class="col-12"><tr>Pasangan Kamu : <b>`+nama2+`</b></tr></div>
+                <div class=col-4>Tanggal Lahir : `+majnun[0][0]+` `+majnun[0][1]+` `+majnun[0][2]+`</div>
+                <div class=col-4>Pasaran : `+majnun[1]+ ` `+majnun[2]+` </div>
+                <div class=col-4>Neptu  : `+majnun[3]+`  </div>
+</div>
+</div><br><div class="menu">Jadi Neptu Kalian <b>`+layla[3]+`</b> dan <b>`+majnun[3]+`</b> wetonnya  <b>`+total+`</b></div>    
+            <div class="row mt-3">&nbsp &nbsp &nbsp
                 <div class="col-12 hasil" id="hasil" style="background-color: #`+final[2]+`;">
-                    <b>`+final[0]+`</b> `+layla[2]+`
-                    <p>
-                        &nbsp &nbsp &nbsp `+final[1]+`
-                    </p>
+                       <center><br><strong>`+final[0]+`  (`+total+`) </strong> </center>
+                    <p><blockquote>
+                         `+final[1]+`
+                    </blockquote></p><span class="text" style="font-size: 0.7em"><a href="https://www.panjinawangkung.com">panjinawangkung.com</a></span>
                 </div>
             </div>
         </div>
