cc6d67fdf070c35c8336bad300f7f86e9c867bb9
kenthusblog <47094667+kenthusblog@users.noreply.github.com>
2021-01-11 01:59:10 +0700

    Update neptu.js

--- neptu.js
+++ neptu.js
@@ -138,16 +138,14 @@ $('#submit').on('click', function () {
    $("#result").html("");
 
     var nama1 = $('#inputNama1').val();
-    var nama2 = $('#inputNama2').val();	
 
     var tanggal1 = $('#inputTanggal1').val();
     var tanggal2 = $('#inputTanggal2').val();
     
     var layla = Hitung(tanggal1);
 
-    var majnun = Hitung(tanggal2);
     
-    var total = layla[3]+majnun[3];
+    var total = layla[3];
     var final = weton(total)
 
     $("#result").append(`                    
