7cc8643dcdb9ce64910a195e20ea3e280d8fedf2
kenthusblog <47094667+kenthusblog@users.noreply.github.com>
2021-01-11 03:22:57 +0700

    Update neptu.js

--- neptu.js
+++ neptu.js
@@ -153,9 +153,7 @@ $('#submit').on('click', function () {
             <div class="row mt-3">&nbsp &nbsp &nbsp
                 <div class="col-12 hasil" id="hasil" style="background-color: #`+final[2]+`;">
                        <center><br><strong>`+final[0]+`  (`+total+`) </strong> </center>
-                    <p><blockquote>
-                         `+final[1]+`
-                    </blockquote></p><span class="text" style="font-size: 0.7em"><a href="https://www.panjinawangkung.com">panjinawangkung.com</a></span>
+                    <span class="text" style="font-size: 0.7em"><a href="https://www.panjinawangkung.com">panjinawangkung.com</a></span>
                 </div>
             </div>
         </div>
